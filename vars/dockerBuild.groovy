e926d4b789d418e2bed01b0bd77b21321b7dacf4
Kasireddy sairam <151829155+kasireddysairam@users.noreply.github.com>
2024-12-15 13:33:34 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,1 +1,8 @@
+def call(String project,String ImageTag,String hubuser) {
+sh """
+docker image build -t   ${hubuser}/${project} .
+docker image tag   ${hubuser}/${project}  ${hubuser}/${project}:${ImageTag}
+docker image tag   ${hubuser}/${project}  ${hubuser}/${project}:latest
+"""
+}
 
