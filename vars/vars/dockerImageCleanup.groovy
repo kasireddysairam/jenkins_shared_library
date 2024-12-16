d1fadf72bf6b6ac82531fa8f6ba5e41216c0ccf5
Kasireddy sairam <151829155+kasireddysairam@users.noreply.github.com>
2024-12-16 22:26:54 +0530

    Delete vars/vars directory

--- vars/vars/dockerImageCleanup.groovy
+++
@@ -1,7 +0,0 @@
-def call(String project, String ImageTag, String hubUser){
-    
-    sh """
-    docker rmi ${hubUser}/${project}:${ImageTag}
-     docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
-    """
-}
