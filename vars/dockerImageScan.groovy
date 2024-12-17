125c97f1213b3c17c31d020f2dd28a3b0c327452
Kasireddy sairam <151829155+kasireddysairam@users.noreply.github.com>
2024-12-17 10:12:16 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,13 +1,6 @@
-def call(String project, String ImageTag, String hubUser) {
-   try {
-       sh """
-       echo "Scanning image: ${hubUser}/${project}:${ImageTag}"
-       trivy image ${hubUser}/${project}:${ImageTag} > scan.txt
-       echo "Scan results saved to scan.txt"
-       cat scan.txt
-       """
-   } catch (Exception e) {
-       echo "Trivy scan failed: ${e.getMessage()}"
-       error("Image scan failed. Please check the logs for details.")
-   }
-}
+def call(String project, String ImageTag, String hubUser){
+    
+    sh """   
+      trivy image ${hubUser}/${project}:${imageTag}> scan.txt
+    cat scan.txt
+    """ }
