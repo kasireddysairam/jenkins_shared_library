df962dc00fec0dd8346b8464ad36600d827e0197
Kasireddy sairam <151829155+kasireddysairam@users.noreply.github.com>
2024-12-17 10:16:19 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,6 @@
 def call(String project, String ImageTag, String hubUser){
     
     sh """   
-      trivy image ${hubUser}/${project}:${imageTag}> scan.txt
+      trivy image ${hubUser}/${project}:${ImageTag}> scan.txt
     cat scan.txt
     """ }
